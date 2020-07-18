3e4f62edbb7a63d9a374990ae111b78932542740
William Trout <troutw5593@gmail.com>
2020-07-18 10:12:25 -0500

    fixed __dirname. will now run

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,15 +3,15 @@ const path = require("path");
 const router = require("express").Router();
 
 router.get("/", function (req, res) {
-  res.sendFile(path.join(_dirname, "../public/index.html"));
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 router.get("/exercise", (req, res) => {
-  res.sendFile(path.join(_dirname, "../public/exercise.html"));
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
 router.get("/stats", (req, res) => {
-  res.sendFile(path.join(_dirname, "../public/stats.html"));
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
 
 module.exports = router;
